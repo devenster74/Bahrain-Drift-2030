feddf7a1c76fc17711a10f04894efd687d597087
Deven Setpal <96651972+devenster74@users.noreply.github.com>
2023-12-11 23:42:50 +0300

    FlipCar.cs

--- FlipCar.cs
+++ FlipCar.cs
@@ -2,23 +2,18 @@ using UnityEngine;
 
 public class FlipCar : MonoBehaviour
 {
-    // Store the original rotation
+
     private Quaternion originalRotation;
 
-    // Use this for initialization
     void Start()
     {
-        // Save the original rotation of the object
         originalRotation = transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Check if the 'F' key was pressed
         if (Input.GetKeyDown(KeyCode.F))
         {
-            // Reset the object's rotation to the original rotation
             transform.rotation = originalRotation;
         }
     }
